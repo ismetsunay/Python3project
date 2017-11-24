19e8469a80cf65da4db3ada77b5de8046a69205b
ismetsunay <32555601+ismetsunay@users.noreply.github.com>
2017-11-24 21:11:05 +0100

    Delete İsimoluşturucu.py

--- .gitignore/İsimoluşturucu.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def isimler():
-    isim = ["Albert", "Charles", "Nicolas", "Michael", "Anders", "Isaac", "Stephen", "Marie", "Richard"]
-    soyadı = ["Einstein", "Darwin", "Copernicus", "Faraday", "Celsius", "Newton", "Hawking", "Curie","Dawkins"]
-    return "{} {}".format(random.choice(isim), random.choice(soyadı))
-for i in range(5):
-    print(isimler())
